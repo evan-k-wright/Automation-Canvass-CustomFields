2ee69bd693507fef5afc0746ef328a7a01017c71
Evan Wright <rideburtonsnow3@gmail.com>
2018-11-30 14:02:28 -0700

    adding more code

--- pageObjects/addCustomField.js
+++ pageObjects/addCustomField.js
@@ -2,13 +2,15 @@ module.exports = {
     url: 'https://admin.saleshub.io/users/login',
     elements: {
         //login
+            canvassApp: 'a[class="enabled app-canvass"]',
+            canvass: 'div[class="current-app"]',
             companyURL: 'input[placeHolder="Company URL"]',
             loginButton: 'button[type="submit"]', 
             password: 'input[placeHolder="Password"]',
             salesHub: 'div[class="current-app"]',
             username: 'input[placeHolder="Username"]',
 
-        
-
+        //custom fields
+            
     }
 }
--- testing/customFieldsTest.js
+++ testing/customFieldsTest.js
@@ -6,22 +6,27 @@ module.exports = {
         //browser.url('https://admin.saleshub.io/users/login')
     },
     after: browser => {
-        browser.end()
+        browser
     },
 
     'Add Custom Field': (browser) => {
         //functions.login(browser, data)
         let addCustomField = browser.page.addCustomField();
         addCustomField.navigate()
-            .waitForElementVisible('@companyURL', 5000)
-            .waitForElementVisible('@username', 5000)
-            .waitForElementVisible('@password', 5000)
-            .setValue('@companyURL', data.company)
-            .setValue('@username', data.username)
-            .setValue('@password', data.password)
-            .waitForElementVisible('@loginButton', 5000)
-            .click('@loginButton')
-            .waitForElementVisible('@salesHub', 5000)
-            
+            //login
+                .waitForElementVisible('@companyURL', 5000)
+                .waitForElementVisible('@username', 5000)
+                .waitForElementVisible('@password', 5000)
+                .setValue('@companyURL', data.company)
+                .setValue('@username', data.username)
+                .setValue('@password', data.password)
+                .waitForElementVisible('@loginButton', 5000)
+                .click('@loginButton')
+                .waitForElementVisible('@salesHub', 5000)
+                .click('@salesHub')
+                .waitForElementVisible('@canvassApp', 5000)
+                .click('@canvassApp')
+                .waitForElementVisible('@canvass', 5000)
+
     }
 }
